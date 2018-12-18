da76fee41515f4e1598857b04feca289e8ca51f2
Computer <computer@Computers-MacBook-Pro.local>
2018-12-17 17:53:21 -0700

    data structures

---
+++ app.js
@@ -0,0 +1,24 @@
+//1
+//a boolean
+//a string
+//an array
+//an object
+//an object
+//an object
+
+// take it easy
+//1
+const rainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+//2
+console.log(rainbow[4]);
+
+//3
+const dillon = {
+    food: "calamari",
+    hobby: "Disc golf",
+    town: "Boulder",
+    datatype: "boolean"
+};
+
+console.log(dillon.hobby);
